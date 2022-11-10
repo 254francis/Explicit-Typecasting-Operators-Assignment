a1726039f69cb19b04f31ef080ced46a6f4ef8fd
254francis <86851202+254francis@users.noreply.github.com>
2022-11-10 20:02:55 +0300

    Add files via upload

---
+++ exercise2 Operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+float base, height,area;
+
+cout<<"Enter Base\n";
+cin>>base;
+cout<<"Enter Height\n";
+cin>>height;
+area = (base*height)/0.5;
+cout<<“The area is:"<<area<<"\n";//output the area
+return 0;
+}
