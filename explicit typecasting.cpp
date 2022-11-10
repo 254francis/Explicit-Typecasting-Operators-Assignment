0afc27c4229c0dbaaf44addf2ea8d1b4e6d58a34
254francis <86851202+254francis@users.noreply.github.com>
2022-11-10 19:23:03 +0300

    Add files via upload

---
+++ explicit typecasting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//Declaring Variables
+
+float myfloat = 10.123;
+int myNum;
+myNum = static_cast<int>(myfloat);   // float to integer
+
+return 0;
+}
